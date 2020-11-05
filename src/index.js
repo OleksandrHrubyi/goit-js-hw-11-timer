af60703ed70a91544e2b823524cabbd6a70f81a8
OleksandrGrubyi <ghrubyi@ukr.net>
2020-11-05 03:42:26 +0200

    add 11hw timer (draft)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import './styles.css';
 
 
+const ref = {
+    valueDay: document.querySelector('.value[data-value="days"]'),
+    valueHours: document.querySelector('.value[data-value="hours"]'),
+    valueMinutes: document.querySelector('.value[data-value="mins"]'),
+    valueSecs: document.querySelector('.value[data-value="secs"]')
+    
+}
 
 
 
 
+const timer = {
+    start() {
+        const startTime = new Date('Jan 31, 2021');
+        
+        
+        
+        
+        setInterval(() => {
+            const currentTime = Date.now();
+            const timeLeft = startTime - currentTime;
+            const {days, hours, mins, secs} = getTimerComponents(timeLeft);
+            ref.valueDay.textContent = `${days}`;
+            ref.valueHours.textContent = `${hours}`;
+            ref.valueMinutes.textContent = `${mins}`;
+            ref.valueSecs.textContent = `${secs}`
+        }, 1000)
+
+    }
+}
+
+timer.start()
+
+function pad(value) {
+    return String(value).padStart(2, '0')
+}
+
+function getTimerComponents(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return {days, hours, mins, secs}
+}
 
